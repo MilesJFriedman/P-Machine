f28cbd258c2e3669b6d06edc97572ef1183b661f
Miles Friedman <milesfriedmanfl@gmail.com>
2014-06-07 02:08:28 -0700

    Successfully prints out the instructions in assembly format

--- P-Machine.c
+++ P-Machine.c
@@ -24,18 +24,22 @@ void executeCycle (); //executes every direction in the IR array.
 void fetchCycle (); //fetches the instruction from the input file and stores it into the IR.
 void printIRArray (); //prints the instruction register array that holds each instruction register.
 int countIntsInFile (); //counts the number of lines in the input file.
+void displayAssemblyInstructions (); //displays the instructions as assembly
+void setAssemblyInstructions (); //sets the assembly instructions
+
+
+int SP = 0; //stack pointer
+int BP = 1; //base pointer
+int PC = 0; //program counter
+//create and initialize the stack
+int stack[MAX_STACK_HEIGHT] = {0};
 
 
 int main (int argc, char *argv[]) {
-	int SP = 0; //stack pointer
-	int BP = 1; //base pointer
-	int PC = 0; //program counter
+
 	//instruction IR; //instruction register
 	int intCount = 0; //the number of ints contained within the file.
 	int instructionCount = 0; //the number of instructions that must be fetched and executed.
-		
-	//create and initialize the stack
-	int stack[MAX_STACK_HEIGHT] = {0};
 	
 	//initialize each register in the register file to 0
 	int registerFile[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -49,7 +53,17 @@ int main (int argc, char *argv[]) {
 	
 	//fetches the instruction from the input file
 	fetchCycle(IR, instructionCount);
-	printIRArray(IR, instructionCount);
+	//printIRArray(IR, instructionCount);
+	
+	//print out the header for the assembly that will be printed after each instruction is read in during the
+	//fetch cycle.
+	printf("Line OP R L M \n");
+	
+	char opCodeString[3]; //a string representing the instruction that corresponds to the opCode.
+	displayAssemblyInstructions(IR, instructionCount);
+	
+	//printf("\n\nInitial Values PC BP SP \n");
+	//executeCycle (IR, instructionCount, registerFile);
 	
 	//printf("intCount: %d \n", intCount);
 	//printf("instructionCount: %d \n", instructionCount);
@@ -59,14 +73,100 @@ int main (int argc, char *argv[]) {
 
 
 //Function Definitions:
-void executeCycle (instruction *IR, int instructionCount, registerFile, stack) {
+void setAssemblyInstructions (instruction *IR, int instructionCount, char *opCodeString, int i) {
+		//this switch fills the opCodeString
+		switch (IR[i].op) {
+			case 1: //opCodeString = "LIT";
+					strcpy(opCodeString, "LIT");
+					break;
+			case 2: //opCodeString = "RTN";
+					strcpy(opCodeString, "RTN");
+					break;
+			case 3: //opCodeString = "LOD";
+					strcpy(opCodeString, "LOD");
+					break;
+			case 4: //opCodeString = "STO";
+					strcpy(opCodeString, "STO");
+					break;
+			case 5: //opCodeString = "CAL";
+					strcpy(opCodeString, "CAL");
+					break;
+			case 6: //opCodeString = "INC";
+					strcpy(opCodeString, "INC");
+					break;
+			case 7: //opCodeString = "JMP";
+					strcpy(opCodeString, "JMP");
+					break;
+			case 8: //opCodeString = "JPC";
+					strcpy(opCodeString, "JPC");
+					break;
+			case 9: //opCodeString = "SIO";
+					strcpy(opCodeString, "SIO");
+					break;
+			case 10: //opCodeString = "SIO";
+					 strcpy(opCodeString, "SIO");
+					 break;
+			case 11: //opCodeString = "SIO";
+					 strcpy(opCodeString, "SIO");
+					 break;
+			case 12: //opCodeString = "NEG";
+					 strcpy(opCodeString, "NEG");
+					 break;
+			case 13: //opCodeString = "ADD";
+					 strcpy(opCodeString, "ADD");
+					 break;
+			case 14: //opCodeString = "SUB";
+					 strcpy(opCodeString, "SUB");
+					 break;
+			case 15: //opCodeString = "MUL";
+					 strcpy(opCodeString, "MUL");
+					 break;
+			case 16: //opCodeString = "DIV";
+					 strcpy(opCodeString, "DIV");
+					 break;
+			case 17: //opCodeString = "ODD";
+					 strcpy(opCodeString, "ODD");
+					 break;
+			case 18: //opCodeString = "MOD";
+					 strcpy(opCodeString, "MOD");
+					 break;
+			case 19: //opCodeString = "EQL";
+					 strcpy(opCodeString, "EQL");
+					 break;
+			case 20: //opCodeString = "NEQ";
+					 strcpy(opCodeString, "NEQ");
+					 break;
+			case 21: //opCodeString = "LSS";
+					 strcpy(opCodeString, "LSS");
+					 break;
+			case 22: //opCodeString = "LEQ";
+					 strcpy(opCodeString, "LEQ");
+					 break;
+			case 23: //opCodeString = "GTR";
+					 strcpy(opCodeString, "GTR");
+					 break;
+			case 24: //opCodeString = "GEQ";
+					 strcpy(opCodeString, "GEQ");
+					 break;
+		}
+}
+
+void displayAssemblyInstructions (instruction *IR, int instructionCount, char *opCodeString) {	
+	int i = 0;
+	for (i = 0; i < instructionCount; i++) {
+		setAssemblyInstructions (IR, instructionCount, opCodeString, i);
+		printf("%d %s %d %d %d \n", i, opCodeString, IR[i].r, IR[i].l, IR[i].m);
+	}
+}
+
+void executeCycle (instruction *IR, int instructionCount, int *registerFile) {
 	int halt = 0;
 	int i = 0;
 	for (i = 0; i < instructionCount; i++) {
 		switch (IR[i].op) {
 			//LIT R, 0, M
 			case 1: registerFile[IR[i].r] = IR[i].m;
-					break
+					break;
 			//RTN 0, 0, 0
 			case 2: SP = BP - 1;
 					BP = stack[SP + 3];
@@ -127,24 +227,27 @@ void executeCycle (instruction *IR, int instructionCount, registerFile, stack) {
 			case 18: registerFile[IR[i].r] = registerFile[IR[i].l] % registerFile[IR[i].m];
 					 break;
 			//EQL
-			case 19: registerFile[IR[i].r] = (registerFile[IR[i].l] == registerFile[IR[i].m];
+			case 19: registerFile[IR[i].r] = (registerFile[IR[i].l] == registerFile[IR[i].m]);
 					 break;
 			//NEQ
-			case 20: registerFile[IR[i].r] = (registerFile[IR[i].l] != registerFile[IR[i].m];
+			case 20: registerFile[IR[i].r] = (registerFile[IR[i].l] != registerFile[IR[i].m]);
 					 break;
 			//LSS
-			case 21: registerFile[IR[i].r] = (registerFile[IR[i].l] < registerFile[IR[i].m];
+			case 21: registerFile[IR[i].r] = (registerFile[IR[i].l] < registerFile[IR[i].m]);
 					 break;
 			//LEQ
-			case 22: registerFile[IR[i].r] = (registerFile[IR[i].l] <= registerFile[IR[i].m];
+			case 22: registerFile[IR[i].r] = (registerFile[IR[i].l] <= registerFile[IR[i].m]);
 					 break;
 			//GTR
-			case 23:  registerFile[IR[i].r] = (registerFile[IR[i].l] > registerFile[IR[i].m];
+			case 23:  registerFile[IR[i].r] = (registerFile[IR[i].l] > registerFile[IR[i].m]);
 					 break;
 			//GEO
-			case 24: registerFile[IR[i].r] = (registerFile[IR[i].l] >= registerFile[IR[i].m];
+			case 24: registerFile[IR[i].r] = (registerFile[IR[i].l] >= registerFile[IR[i].m]);
 					 break;
 		}
+		
+		//setAssemblyInstructions
+		//printf("%d %s %d %d %d %d %d %d | %d %d %d %d %d %d \n", i, opCodeString, IR[i].r, IR[i].l, IR[i].m, PC, BP, SP, stack[0], stack[1], stack[2], stack[3], stack[4], stack[5]);
 	}
 }
 
@@ -173,7 +276,7 @@ void printIRArray (instruction *IR, int instructionCount) {
 	}
 }
 
-int findBase (l, base) {} //l stands for L in the instruction format
+int base (l, base) { //l stands for L in the instruction format.
 	int b1; //find base L levels down
 	b1 = base;
 	while (1 > 0) {
